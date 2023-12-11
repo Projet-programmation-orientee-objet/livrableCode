3751e2ba30ff65d4fe00c03052790403f8cff25d
Ayasalem127 <150123714+Ayasalem127@users.noreply.github.com>
2023-12-11 09:11:47 +0100

    Update CNX.h

--- CNX.h
+++ CNX.h
@@ -1,22 +1,29 @@
 #pragma once
+using namespace System;
+using namespace System::Collections::Generic;
+using namespace System::Data;
+using namespace System::Data::SqlClient;
 namespace Composant
 {
-    ref class CNX
-    {
-    private:
-        System::String^ sSql;
-        System::String^ sCnx;
-        System::Data::SqlClient::SqlConnection^ oCnx;
-        System::Data::SqlClient::SqlCommand^ oCmd;
-        System::Data::SqlClient::SqlDataAdapter^ oDA;
-        System::Data::DataSet^ tabClient;
-        System::Data::DataSet^ tabAdress;
-        System::Data::DataSet^ tabCommand;
-    public:
-        CNX();
-        System::Data::DataSet^ getData(System::String^ sSql, System::String^ sDataTableName);
-        int modifyData(System::String^);
-        void modifyDataNONSCALAr(System::String^);
-        System::Data::DataSet^ gettabClient() { return this->tabClient; }
-    };
+	ref class CNX
+	{
+	private:
+		System::String^ sSql;
+		System::String^ sCnx;
+		System::Data::SqlClient::SqlConnection^ oCnx;
+		System::Data::SqlClient::SqlCommand^ oCmd;
+		System::Data::SqlClient::SqlDataAdapter^ oDA;
+		System::Data::DataSet^ tabClient;
+		System::Data::DataSet^ tabAdress;
+		System::Data::DataSet^ tabCommand;
+	public:
+		CNX();
+		System::Data::DataSet^ getData(System::String^ sSql, System::String^ sDataTableName);
+		List<Tuple<int, int>^>^ Composant::CNX::GetList(String^ , String^ , String^ );
+		int modifyData(System::String^);
+		void modifyDataNONSCALAr(System::String^);
+		System::Data::DataSet^ gettabClient() { return this->tabClient; }
+	};
 }
+
+
