3a9ac5105db8d4e7c5652c1700f9d973aaca6f7e
Ayasalem127 <150123714+Ayasalem127@users.noreply.github.com>
2023-12-11 14:23:27 +0100

    Update MappingAdress.h

--- MappingAdress.h
+++ MappingAdress.h
@@ -18,6 +18,7 @@ namespace Composant
 		void setID_Adress(int);
 		void setstreet(String^);
 		void setnumber(int);
+		void setCity(String^ city);
 		void setID_Customer(int);
 		void setID_city(int);
 		void setID_country(int);
@@ -28,9 +29,11 @@ namespace Composant
 		System::String ^ Insert(void);
 		System::String^ InsertCity(void);
 		System::String^ InsertCountry(void);
+		System::String^ InsertLive(void);
 		System::String ^ Delete(void);
 		System::String^ DeleteCity(void);
 		System::String^ DeleteCountry(void);
+		System::String^ DeleteLive(void);
 		System::String ^ Update(void);
 		System::String^ UpdateCountry(void);
 		System::String^ UpdateCity(void);
